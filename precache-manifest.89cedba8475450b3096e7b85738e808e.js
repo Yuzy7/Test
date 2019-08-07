db3d47c115daea381ab5abbe45a693a3fa066260
Yuzhou Wang <wangyuzhou777@gmail.com>
2019-08-06 21:34:11 -0400

    Updates

---
+++ precache-manifest.89cedba8475450b3096e7b85738e808e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "78ad0f0ae6d307dd3c40ea55e83de6ca",
+    "url": "/Test/static/media/news2.78ad0f0a.jpeg"
+  },
+  {
+    "revision": "7c9bda5bfac80de86538",
+    "url": "/Test/static/css/main.2fbaedc1.chunk.css"
+  },
+  {
+    "revision": "8eb059f053945c691082",
+    "url": "/Test/static/js/runtime~main.e55b42b1.js"
+  },
+  {
+    "revision": "806f65a6abfd35648a77",
+    "url": "/Test/static/js/2.ccf9dac7.chunk.js"
+  },
+  {
+    "revision": "396f37293d1b272b616967077f032e0a",
+    "url": "/Test/static/media/logo.396f3729.png"
+  },
+  {
+    "revision": "7c9bda5bfac80de86538",
+    "url": "/Test/static/js/main.6a06d751.chunk.js"
+  },
+  {
+    "revision": "d5492d6c564c2770f88a0df9be618098",
+    "url": "/Test/static/media/Wechat.d5492d6c.jpeg"
+  },
+  {
+    "revision": "59cf3f438b44aef23e423a1aef271528",
+    "url": "/Test/static/media/Connecting.59cf3f43.jpg"
+  },
+  {
+    "revision": "8543b26c350bb967f4feaeb79506a8a5",
+    "url": "/Test/static/media/Culture influencer.8543b26c.jpg"
+  },
+  {
+    "revision": "46d4f952d310464ec2ab437af61ca8c2",
+    "url": "/Test/static/media/background.46d4f952.jpg"
+  },
+  {
+    "revision": "c55b506a667039e13b2891747fa5e045",
+    "url": "/Test/index.html"
+  }
+];
